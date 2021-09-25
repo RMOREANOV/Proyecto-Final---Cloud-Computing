85c29aa08445e903b56df868479ab3f1a3631901
RMOREANOV <30770842+RMOREANOV@users.noreply.github.com>
2021-09-24 21:26:40 -0500

    Create server.js

---
+++ backend/medi_backend/server.js
@@ -0,0 +1,15 @@
+const strapi = require('strapi');
+
+// start strapi application
+strapi().start();
+
+
+/my-project
+└─── api
+└─── build
+└─── config
+└─── extensions
+└─── public
+- favicon.ico
+- package.json
+- server.js
